ab812060c7a48d6da1e07a7d6bf493f5253a5695
MasqueradeOfSilence <alexcn711@gmail.com>
2022-12-16 21:35:03 -0700

    mac commit, need to consolidate files

---
+++ MLS-MPM Solver/MLS-MPM Solver/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MLS-MPM Solver
+//
+//  Created by Alex Neville on 12/16/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
